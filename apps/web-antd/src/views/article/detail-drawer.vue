78cf168070b86341129d1a93de11bc542a6d1499
JasonQM <695881709@qq.com>
2026-09-17 12:33:11 +0800

    style(article,table): 调整保证人表格列宽与滚动宽度
    优化暗色模式下表行高亮背景色，提升文字可读性
    调整保证人表格各列宽度与横向滚动阈值，适配内容排版

--- apps/web-antd/src/views/article/detail-drawer.vue
+++ apps/web-antd/src/views/article/detail-drawer.vue
@@ -1102,16 +1102,16 @@ const supplyColumns = [
                         <Table
                           v-if="g.type === 'guarantor'"
                           :columns="[
-                            { title: '保证人', dataIndex: 'name', width: 240, ellipsis: true },
-                            { title: '类型', dataIndex: 'genre_display', width: 60, align: 'center' },
-                            { title: '联系地址', dataIndex: 'address', width: 220, ellipsis: true },
+                            { title: '保证人', dataIndex: 'name', width: 180, ellipsis: true },
+                            { title: '类型', dataIndex: 'genre_display', width: 40, align: 'center' },
+                            { title: '联系地址', dataIndex: 'address', width: 280, ellipsis: true },
                             { title: '联系人', dataIndex: 'contact_name', width: 60 },
-                            { title: '联系电话', dataIndex: 'contact_phone', width: 130 },
-                            { title: '操作', key: 'op', width: 80, align: 'center' },
+                            { title: '联系电话', dataIndex: 'contact_phone', width: 70 },
+                            { title: '操作', key: 'op', width: 40, align: 'center' },
                           ]"
                           :data-source="g.items as GuarantorItem[]"
                           :pagination="false"
-                          :scroll="{ x: 790 }"
+                          :scroll="{ x: 670 }"
                           :row-key="(_, idx) => `${g.key}-g-${idx}`"
                           size="small"
                         >
